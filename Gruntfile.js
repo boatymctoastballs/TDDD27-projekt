59fa1ff63c6e5012c0255c8e98800d631814e72a
boatymctoastballs <cittte@gmail.com>
2017-03-27 21:03:54 +0200

    grunt build & structure

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+
+module.exports = function(grunt){
+
+grunt.registerTask()
+	grunt.initConfig({
+  		concat: {
+    		js: {
+	      		src: ['js/app.js'],
+	      		dest: 'build/js/scripts.js',
+    		},
+    		css: {
+	      		src: ['css/bootstrap/bootstrap.css','css/main.css'],
+	      		dest: 'build/css/fullCss.css',
+    		},
+  		},
+  		watch: {
+			js: {
+				files: ['js/**/*.js'],
+				tasks: ['concat:js'],
+			},
+			css: {
+				files: ['css/**/*.css'],
+				tasks: ['concat:css'],
+			},
+		},
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.registerTask('default', ['concat', 'watch'])
+};
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # TDDD27-projekt
 Webb app
+
+Front end: AngularJS, bootstrap/ ui bootstrap
+
+back end: PHP - micro framework Lumen (Derived from Laravel), MySQL for database management
+
+Wamp server service
+
+Grunt for building 
